eba7a838bbe3d7adb825b3fbd3ef028e2f76f595
taechsantiago <santiago.tabordae@udea.edu.co>
2021-11-27 08:53:58 -0500

    Cambio de fondo
    Se carga la imagen que se plantea como fondo para la ventana principal del juego, en el .py que registran los respectivos comentarios y en el documento "usedFunctions" la explicación a las funciones de pygame usadas

--- basicGame.py
+++ basicGame.py
@@ -25,22 +25,38 @@ import pygame  #Conjunto de modulos de python diseñado para el desarrollo de vi
 pygame.init()
 
 #---- Se define las dimensiones de la ventana principal del juego -------------------------------
-SCREEN_WIDTH = 400   #ancho
-SCREEN_HEIGHT = 600  #alto
+SCREEN_WIDTH = 512  #ancho
+SCREEN_HEIGHT = 512  #alto
 
 #---- Se crea la ventana principal del juego ----------------------------------------------------
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))#creación de la ventana pygame
 pygame.display.set_caption('headUpGame')                       #cambio del titulo de la ventana
 
+#---- Se carga la imagen de fondo para la ventana y se configura para mantener la posible -------
+#---- transparencia de la imagen png cargada ----------------------------------------------------
+backGroundImage = pygame.image.load('./assets/smallBlueNebula.png').convert_alpha()
+
+
+
+#------------------------------------------------------------------------------------------------
+#-- 3. Logica para el videojuego ----------------------------------------------------------------
+#------------------------------------------------------------------------------------------------
+
 #---- Se define el ciclo con el cual se evita el cierre de la ventana principal creada ----------
 playing = True #Variable de control para el ciclo mencionado
 while playing:
+    #------ Cambio del fondo de la ventana  -----------------------------------------------------
+    screen.blit(backGroundImage, (0,0)) #Cambia los pixeles de la ventana, (recurso, coordenada)
+
     #------ Manejo de eventos  ------------------------------------------------------------------
     #------ pygame.event.get() obtendrá todos los eventos y los eliminará de la cola ------------
     for event in pygame.event.get():
         #------ el evento es de tipo QUIT cuando se presiona el boton "X" de la ventana ---------
         if event.type == pygame.QUIT:
             playing = False #Se termina el ciclo para permitir el cierre de la ventana
+
+    #------ Actualización grafica de la ventana  ------------------------------------------------
+    pygame.display.update() #Actualiza la ventana, al pasar parametro actualiza una porción
 
 pygame.quit() #Desinicializa todos los módulos de pygame que han sido previamente inicializados.
 #------------------------------------------------------------------------------------------------
