382a7d5bcbb63bd350f0744d85128e325e2d0108
ManueloRZ2 <nuelarbelaez@gmail.com>
2022-01-30 10:24:55 -0500

    Opencv
    Se agrega el movimiento con reconocimiento de objetos por color, se adicionan lineas de control de movimiento

--- OpenCV.py
+++ OpenCV.py
@@ -18,15 +18,18 @@ while True:
   mask = cv2.inRange(image, lower, upper)       #encuentra el color escogido en las imagenes de la camara
 
 
-#----------------------------------------- 2. contorno de la figura ---------------------------------------------
-#
-# las siguienets encuentra las coordenadas del color detectado, CHAIN_APPROX_SIMPLE permite tomar los puntos necesarios
-# del contorno no toma cada punto de las coordenadas, RETR_EXTERNAL toma el contorno externo de la forma con el color
+  #----------------------------------------- 2. contorno de la figura ---------------------------------------------
+  #
+  # las siguienets encuentra las coordenadas del color detectado, CHAIN_APPROX_SIMPLE permite tomar los puntos necesarios
+  # del contorno no toma cada punto de las coordenadas, RETR_EXTERNAL toma el contorno externo de la forma con el color
 
   contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)  
 
   if len(contours) != 0:
     for contour in contours:
+      if cv2.contourArea(contour) > 50000:
+        print("jump")
+
       if cv2.contourArea(contour) > 1000:        # evita que se pinte el contorno de fragmentos con areas muy pequeñas
         x, y, w, h = cv2.boundingRect(contour)  # genera rectangulo de contorno de la forma
         cv2.rectangle(img, (x,y), (x + w, y + h), (0, 0, 255), 3) # muestra el rectangulo en pantalla
@@ -38,7 +41,7 @@ while True:
         else:
           print('medio')
 
-#-------- se puede borrar luego de terminado -------------------------------------
+  #-------- se puede borrar luego de terminado -------------------------------------
   #cv2.imshow("mask", mask)    # muestra en pantalla la identificación del color
   cv2.imshow("webcam", img)   # muestra los fotogramas captados por la camara
 
